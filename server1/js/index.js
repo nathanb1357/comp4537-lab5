3180e0e08ceea92bf7c4a236026b3545e3b7f010
sbeeve2023 <cgoss3@my.bcit.ca>
2024-10-09 16:05:11 -0700

    Wrote some javascript for the client side index page

--- server1/js/index.js
+++ server1/js/index.js
@@ -2,3 +2,51 @@ document.getElementById('title').innerHTML = MESSAGES.title
 document.getElementById('header').innerHTML = MESSAGES.title
 document.getElementById('insert-button').innerHTML = MESSAGES.insertButton
 document.getElementById('query-button').innerHTML = MESSAGES.queryButton
+
+const serverAddress = 'http://localhost:3000'
+
+document.getElementById('insert-button').addEventListener('click', () => {
+    const data = 'INSERT INTO patient (name, dateOfBirth) VALUES ("Sara Brown", "1901-01-01"), ("John Smith", "1941-01-01"), ("Jack Ma", "1961-01-30"), ("Elon Musk", "1999-01-01")'
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', serverAddress + '/insert', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const responseData = JSON.parse(xhr.responseText);
+            document.getElementById('response').innerHTML = responseData;
+        }
+    };
+    xhr.send(data);
+})
+
+document.getElementById('query-button').addEventListener('click', () => {
+    const query = document.getElementById('query-box').value;
+
+
+    if (query.startsWith('SELECT')) {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', serverAddress + `/query?data=` + query, true);
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                const data = JSON.parse(xhr.responseText);
+                document.getElementById('response').innerHTML = data;
+            }
+        };
+        xhr.send();
+    }
+
+    if (query.startsWith('INSERT')) {
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', serverAddress + '/insert', true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                const responseData = JSON.parse(xhr.responseText);
+                document.getElementById('response').innerHTML = responseData;
+            }
+        };
+        xhr.send(query);
+    }
+})
